a3fb871692c9d61e0528362587789591081c1f33
zahidahmed024 <zahidahmed024@gmail.com>
2020-03-05 01:27:32 +0600

    changes in app.js

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     super(props);
     this.state = {
     };
-    console.log('main :', Store.getState());
+    // console.log('App container :', Store.getState());
     
   }
 
